60ed9c4cefa9d1615268b3d3ce3ff9da7c872f39
Olga Panisova <shadenoff@gmail.com>
2020-08-24 22:59:26 +0300

    Added warning sing when device is not in the landscape position.

---
+++ Assets/Scripts/TurnWarning.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TurnWarning : MonoBehaviour
+{
+
+     Image warningImage;
+
+    private void Awake()
+    {
+        warningImage = GetComponent<Image>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        warningImage.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.deviceOrientation != DeviceOrientation.LandscapeLeft && Input.deviceOrientation != DeviceOrientation.Unknown)
+        {
+            warningImage.enabled = true;
+            Debug.Log("Enabled");
+        }
+        else
+        {
+            warningImage.enabled = false;
+        }
+
+    }
+}
