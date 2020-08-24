b70223d5497f936b14383763780c03a1df500e02
Olga Panisova <shadenoff@gmail.com>
2020-08-24 19:56:58 +0300

    Locked screen in landscape mode.

---
+++ Assets/Scripts/GameInit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInit : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+}
