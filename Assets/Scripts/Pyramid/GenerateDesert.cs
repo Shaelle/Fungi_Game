2ed4e2004b253d5a847b5301579cb2e21e019e78
Olga Panisova <shadenoff@gmail.com>
2020-08-28 21:15:36 +0300

    Added generated desert for the Pyramid level. changed colours of the particles on top of the pyramid, so they now looks more magical.

---
+++ Assets/Scripts/Pyramid/GenerateDesert.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateDesert : MonoBehaviour
+{
+    public GameObject plane;
+
+
+    int halfTilesX = 60;
+    int halfTilesZ = 25;
+    int planeSize = 10;
+
+    Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        startPos = transform.position;
+
+
+        for (int x = -halfTilesX; x < halfTilesX; x++)
+        {
+            for (int z = -halfTilesZ; z < halfTilesZ; z++)
+            {
+                Vector3 pos = new Vector3(
+                    (x * planeSize + startPos.x),
+                    0,
+                    (z * planeSize + startPos.z));
+
+                GameObject t = (GameObject)Instantiate(plane, pos, Quaternion.identity);
+                t.transform.parent = this.transform;
+
+                string tileName = "Tile_" + ((int)(pos.x)).ToString() + "_" + ((int)(pos.z)).ToString();
+                t.name = tileName;
+
+
+            }
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Pyramid/GenerateSandBlock.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateSandBlock : MonoBehaviour
+{
+
+    int heightScale = 1;
+    float detailScale = 5f;
+
+    private void Start()
+    {
+        Mesh mesh = this.GetComponent<MeshFilter>().mesh;
+
+        Vector3[] vertices = mesh.vertices;
+
+
+        for (int v = 0; v < vertices.Length; v++)
+        {
+            vertices[v].y = Mathf.PerlinNoise((vertices[v].x + this.transform.position.x) / detailScale, (vertices[v].z + this.transform.position.z) / detailScale) * heightScale;
+        }
+
+        mesh.vertices = vertices;
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+        this.gameObject.AddComponent<MeshCollider>();
+    }
+}
